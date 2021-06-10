6b27f0eb6405fdc6d40655627d7b98669fe91808
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-10 11:59:48 -0500

    oth

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ fieldnames=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'
 df = pd.read_csv('people.csv')
 df1=df.replace(np.nan,"",regex=True)
 
-data = df1.values.tolist()
 
+data = df1.values.tolist()
+print(data)
 
 
 @app.route('/', methods=["POST","GET"])
